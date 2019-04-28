8bc6c92ad3d8f918d4c810fb5443e0626f5cdf7d
weerayut1996 <weerayut.itsd@gmail.com>
2019-04-28 23:07:25 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const Express = require('express');
+const bodyParser = require('body-parser');
+
+const app = new Express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Running on http://localhost:${port}`);
+});
+
+const login = require('./routes/login'); // 
+app.use('/login', login);
---
+++ routes/login.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const router = express.Router();
+const MongoClient = require("mongodb").MongoClient;
+
+router.get("/", (req, res) => {
+    res.send({
+        status: "login"
+    });
+});
+
+router.post("/c", (req, res) => {
+    var nameVar = req.body.name; // name คือตัวที่พิมลง postman
+    var passwordVar = req.body.password; // password พิมค่าลง postman
+    MongoClient.connect(
+        "mongodb://framework:framework62@ds147096.mlab.com:47096/frameworkdb", {
+            useNewUrlParser: true
+        },
+        function (err, db) {
+            let dbo = db.db("frameworkdb");
+            let userObj = {
+                username: nameVar, // username คือค่า attribute ใน db
+                password: passwordVar // password คือค่า attribute ใน db
+            };
+            dbo.collection("userLoginTable").insertOne(userObj, function (err, result) {
+                if (err) throw err;
+                res.send({
+                    status: "store success",
+                    name: nameVar //response
+                });
+            });
+            db.close();
+        }
+    );
+});
+
+router.post("/register", (req, res) => {
+    var rankVar = req.body.rank;
+    var first_nameVar = req.body.first_name;
+    var last_nameVar = req.body.last_name;
+    var id_milVar = req.body.id_mil;
+    var unit_nameVar = req.body.unit_name;
+    var nameVar = req.body.username; // name คือตัวที่พิมลง postman
+    var passwordVar = req.body.password; // password พิมค่าลง postman
+    MongoClient.connect(
+        "mongodb+srv://weerayut:22374736@cluster0-4wunc.mongodb.net/newDatabase62?retryWrites=true", {
+            useNewUrlParser: true
+        },
+        function (err, db) {
+            if (err) {
+
+                res.sendStatus(404);
+
+                return;
+
+            }
+            let dbo = db.db("newDatabase62");
+            let userObj = {
+                rank: rankVar,
+                first_name: first_nameVar,
+                last_name: last_nameVar,
+                id_mil: id_milVar,
+                unit_name: unit_nameVar,
+                username: nameVar, // username คือค่า attribute ใน db
+                password: passwordVar // password คือค่า attribute ใน db
+            };
+            dbo.collection("userLoginTable").insertOne(userObj, function (err, result) {
+                if (err) {
+
+                    res.send({
+
+                        status: false
+
+                    });
+
+                }
+                res.send({
+                    status: "store success",
+                    name: nameVar //response
+                });
+            });
+            db.close();
+        }
+    );
+});
+
+module.exports = router;
+// test commit Backend..
